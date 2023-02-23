871ec1baa4c983b35393cca1f4afff49fafa320f
KonstantinStanmeyer <konstantin@stanmeyer.com>
2023-02-22 19:34:24 -0700

    home page css finished | projects started

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,11 @@
-import PreviewCard from "../components/PreviewCard"
+import PreviewCard from "./projects/PreviewCard"
 import Link from "next/link";
 
-import { projects } from '../data/projects';
+import { projects } from './projects';
 
 export default function HomePage(){
   return(
-    <div className="mx-auto flex flex-col justify-center mt-8 relative z-40">
+    <div className="mx-auto flex flex-col justify-center mt-2 relative z-40">
       <div className="w-3/5 sm:w-2/5 mx-auto flex flex-row select-none px-5 relative bg-black/30 backdrop-blur-sm rounded-lg py-3 duration-500 transform transition-all translate-y-12 ease-out">
         <div className="transition-opacity ease-in duration-700">
           <h1 className="text-xl sm:text-4xl font-bold text-purple-700 bebas">Konstantin Stanmeyer</h1>
@@ -20,10 +20,34 @@ export default function HomePage(){
       <p className="text-purple-300 text-lg sm:text-xl font-bold my-4 w-4/5 sm:w-2/5 mt-16 mx-auto select-none bg-black/30 backdrop-blur-sm py-2 rounded-lg px-4">
         <span className="bebas text-xl sm:text-2xl text-purple-400 ">ABOUT ME: </span>I'm a full-stack engineer experienced with multiple languages and frameworks including NEXT.js and Ruby on Rails. I love to learn and challenge myself, always striving to improve my logic and approach. Currently living in Colorado. Avid runner and skateboarder.
       </p>
-      <div className="flex flex-col w-1/2">
-        <div></div>
-        <div></div>
+      <div className="flex flex-col w-1/2 mx-auto my-2">
+        <div className="flex flex-wrap items-center justify-center w-full mx-auto">
+          <div className="rounded-full bg-black/30 backdrop-blur-sm w-28 h-28 mx-6 flex justify-center items-center my-2">
+            <img src="/javascript.png" className="w-[80%] h-[80%] rounded-full" />
+          </div>
+          <div className="rounded-full bg-black/30 backdrop-blur-sm w-28 h-28 mx-6 flex justify-center items-center my-2">
+            <img src="/react.png" className="w-[80%] h-[80%] rounded-full" />
+          </div>
+          <div className="rounded-full bg-black/30 backdrop-blur-sm w-28 h-28 mx-6 flex justify-center items-center my-2">
+            <img src="/nextjs.svg" className="w-[80%] h-[80%] rounded-full invert" />
+          </div>
+          <div className="rounded-full bg-black/30 backdrop-blur-sm w-28 h-28 mx-6 flex justify-center items-center my-2">
+            <img src="/nodejs.webp" className="w-[80%] h-[80%] rounded-full" />
+          </div>
+          <div className="rounded-full bg-black/30 backdrop-blur-sm w-28 h-28 mx-6 flex justify-center items-center my-2">
+            <img src="/nodejs.webp" className="w-[80%] h-[80%] rounded-full" />
+          </div>
+          <div className="rounded-full bg-black/30 backdrop-blur-sm w-28 h-28 mx-6 flex justify-center items-center my-2">
+            <img src="/nodejs.webp" className="w-[80%] h-[80%] rounded-full" />
+          </div>
+          <div className="rounded-full bg-black/30 backdrop-blur-sm w-28 h-28 mx-6 flex justify-center items-center my-2">
+            <img src="/nodejs.webp" className="w-[80%] h-[80%] rounded-full" />
+          </div>
+        </div>
       </div>
+      <Link href="/projects" className="w-40 mx-auto h-16 mt-4 rounded-lg bg-purple-800/20 hover:bg-purple-800/40 transition-all duration-300 mb-32 items-center flex justify-center">
+        <p className="text-purple-600 text-3xl bebas">Projects</p>
+      </Link>
       {/* <h2 className="select-none text-4xl mt-16 text-purple-700 font-bold text-center bg-black/30 backdrop-blur-sm w-32 py-2 mx-auto rounded-lg bebas">SKILLS</h2>
       <div className="w-2/5 mx-auto flex flex-col select-none px-5 mb-5 relative bg-black/30 backdrop-blur-sm my-8 py-2 rounded-lg">  
         <h6 className="text-xl text-purple-400"><span className="text-2xl font-bold">Front End:</span> JavaScript | TypeScript | NEXT.js | CSS | Tailwind</h6>
--- app/projects/PreviewCard.jsx
+++ app/projects/PreviewCard.jsx
@@ -6,7 +6,7 @@ export default function ProjectCard({ project }){
     return (
         <div className="w-[21rem] mb-10 transition-all duration-300 h-[23rem] rounded-lg bg-black/80 hover:bg-gray-600 pb-1 mx-auto px-2 relative pt-1 my-6">
             <h2 className="mb-2 select-none w-fit left-1 text-lg font-bold text-gray-200">{projectUrl ? name + " - " : name}{projectUrl ? <a className="hover:text-blue-500 text-base duration-300 transition-all font-semibold" target="_blank" href={projectUrl}>Link</a> : <span className="font-semibold text-base"> - Group Project</span>}</h2>
-            <Image className="rounded-lg h-48 w-full object-cover" width={300} height={200} src={image} />
+            <Image loading="lazy" className="rounded-lg h-48 w-full object-cover" width={300} height={200} src={image} />
             <a className="absolute -bottom-[1.3rem] left-1 text-sm font-bold text-gray-400 transition-all duration-200 hover:text-gray-600" target="_blank" rel="noopener noreferrer" href={frontEnd}>Front End Github</a>
             <a className="absolute -bottom-[1.3rem] right-1 text-sm font-bold text-gray-400 transition-all duration-200 hover:text-gray-600" target="_blank" rel="noopener noreferrer" href={backEnd}>Back End Github</a>
             <p className="select-none text-sm pl-1 pt-1 text-gray-300 font-semibold">{description}</p>
