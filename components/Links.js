ab209cbd8ede5b8282a11a2cad0b6f0a15dbbb17
KonstantinStanmeyer <konstantin@stanmeyer.com>
2023-02-22 18:13:32 -0700

    base setup complete | pre-skills/projects

---
+++ components/Links.js
@@ -0,0 +1,12 @@
+export default function Links(){
+    return(
+        <div className="flex flex-row items-center fixed w-full bottom-10">
+            <div className="flex flex-row items-center justify-between w-1/5 mx-auto">
+                <img className="link hover:hover" src="/resume.png" />
+                <img className="link hover:hover" src="/linkedin.png" />
+                <img className="link hover:hover" src="/github.png" />
+                <img className="link hover:hover" src="/dev.png" />
+            </div>
+        </div>
+    )
+}
