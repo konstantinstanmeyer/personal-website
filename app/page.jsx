e9b443108b408d1caf870143624b79e0d560db10
KonstantinStanmeyer <konstantin@stanmeyer.com>
2023-02-24 10:31:03 -0700

    final changes | relocating projects button

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 import PreviewCard from "./projects/PreviewCard"
 import Link from "next/link";
+import Links from "../components/Links";
 
 import { projects } from './projects';
 
@@ -8,8 +9,9 @@ export default function HomePage(){
     <div className="mx-auto flex flex-col justify-center mt-2 relative z-40">
       <div className="w-3/5 sm:w-2/5 mx-auto flex flex-row select-none px-5 relative bg-black/30 backdrop-blur-sm rounded-lg py-3 duration-500 transform transition-all translate-y-12 ease-out">
         <div className="transition-opacity ease-in duration-700">
-          <h1 className="text-xl sm:text-4xl font-bold text-purple-700 bebas">Konstantin Stanmeyer</h1>
-          <h4 className="text-base sm:text-2xl text-purple-300 font-bold">Software Engineer</h4>
+          <h1 className="text-3xl sm:text-4xl font-bold text-purple-700 bebas -my-1">Konstantin Stanmeyer</h1>
+          <h4 className="text-2xl sm:text-3xl text-purple-500 font-bold -my-1">Software Engineer</h4>
+          <Link href="projects" className="text-xl sm:text-2xl text-purple-300 font-bold mb-1 hover:underline">Projects</Link>
           <div className="flex flex-row items-center font-bold text-gray-400 [&>*]:mr-2 sm:[&>*]:mr-3">
             <a target="_blank" className="text-purple-400 hover:underline bebas sm:text-base text-sm" href="https://github.com/konstantinstanmeyer">Github</a>
             <a target="_blank" className="text-purple-400 hover:underline bebas sm:text-base text-sm" href="https://www.linkedin.com/in/konstantin-stanmeyer/">LinkedIn</a>
@@ -18,42 +20,40 @@ export default function HomePage(){
         </div>
       </div>
       <p className="text-purple-300 text-lg sm:text-xl font-bold my-4 w-4/5 sm:w-2/5 mt-16 mx-auto select-none bg-black/30 backdrop-blur-sm py-2 rounded-lg px-4">
-        <span className="bebas text-xl sm:text-2xl text-purple-400 ">ABOUT ME: </span>I'm a full-stack engineer experienced with multiple languages and frameworks including NEXT.js and Ruby on Rails. I love to learn and challenge myself, always striving to improve my logic and approach. Currently living in Colorado. Avid runner and skateboarder.
+        <span className="bebas text-xl sm:text-2xl text-purple-400 ">ABOUT ME: </span>I'm a full-stack software engineer with a passion for learning in all forms. I love to challenge myself, always striving to improve my process and approach. Experienced with multiple languages and frameworks including NEXT.js and Ruby on Rails. Avid runner and skateboarder.
       </p>
       <div className="flex flex-col lg:w-1/2 w-5/6 md:w-3/5 mx-auto my-2">
         <div className="flex flex-wrap items-center justify-center w-full mx-auto">
-          <div className="rounded-full bg-black/30 backdrop-blur-sm w-28 h-28 md:mx-2 lg:mx-4 flex justify-center items-center my-2 mx-1">
+          <div className="rounded-full bg-black/30 backdrop-blur-sm w-28 h-28 md:mx-2 lg:mx-4 flex justify-center items-center my-2 mx-1 hover:scale-105 transition-all duration-300 hover:brightness-110">
             <img alt="javascript" src="/javascript.png" className="w-[80%] h-[80%] rounded-full" />
           </div>
-          <div className="rounded-full bg-black/30 backdrop-blur-sm w-28 h-28 md:mx-2 lg:mx-4 flex justify-center items-center my-2 mx-1">
+          <div className="rounded-full bg-black/30 backdrop-blur-sm w-28 h-28 md:mx-2 lg:mx-4 flex justify-center items-center my-2 mx-1 hover:scale-105 transition-all duration-300 hover:brightness-110">
             <img alt="react" src="/react.png" className="w-[80%] h-[80%] rounded-full" />
           </div>
-          <div className="rounded-full bg-black/30 backdrop-blur-sm w-28 h-28 md:mx-2 lg:mx-4 flex justify-center items-center my-2 mx-1">
+          <div className="rounded-full bg-black/30 backdrop-blur-sm w-28 h-28 md:mx-2 lg:mx-4 flex justify-center items-center my-2 mx-1 hover:scale-105 transition-all duration-300 hover:brightness-125">
             <img alt="redux" src="/redux.png" className="w-[75%] h-[75%] invert object-cover" />
           </div>
-          <div className="rounded-full bg-black/30 backdrop-blur-sm w-28 h-28 md:mx-2 lg:mx-4 flex justify-center items-center my-2 mx-1">
+          <div className="rounded-full bg-black/30 backdrop-blur-sm w-28 h-28 md:mx-2 lg:mx-4 flex justify-center items-center my-2 mx-1 hover:scale-105 transition-all duration-300 hover:brightness-125">
             <img alt="nextjs" src="/nextjs.svg" className="w-[80%] h-[80%] rounded-full invert" />
           </div>
-          <div className="rounded-full bg-black/30 backdrop-blur-sm w-28 h-28 md:mx-2 lg:mx-4 flex justify-center items-center my-2 mx-1">
+          <div className="rounded-full bg-black/30 backdrop-blur-sm w-28 h-28 md:mx-2 lg:mx-4 flex justify-center items-center my-2 mx-1 hover:scale-105 transition-all duration-300 hover:brightness-125">
             <img alt="mongodb" src="/mongodb.webp" className="w-[70%] h-[70%]" />
           </div>
-          <div className="rounded-full bg-black/30 backdrop-blur-sm w-28 h-28 md:mx-2 lg:mx-4 flex justify-center items-center my-2 mx-1">
+          <div className="rounded-full bg-black/30 backdrop-blur-sm w-28 h-28 md:mx-2 lg:mx-4 flex justify-center items-center my-2 mx-1 hover:scale-105 transition-all duration-300 hover:brightness-110">
             <img alt="ruby" src="/ruby.png" className="w-[80%] h-[80%] rounded-full" />
           </div>
-          <div className="rounded-full bg-black/30 backdrop-blur-sm w-28 h-28 md:mx-2 lg:mx-4 flex justify-center items-center my-2 mx-1">
+          <div className="rounded-full bg-black/30 backdrop-blur-sm w-28 h-28 md:mx-2 lg:mx-4 flex justify-center items-center my-2 mx-1 hover:scale-105 transition-all duration-300 hover:brightness-110">
             <img alt="ruby-on-rails" src="/rails.png" className="w-[80%] h-[80%] rounded-full" />
           </div>
-          <div className="rounded-full bg-black/30 backdrop-blur-sm w-28 h-28 md:mx-2 lg:mx-4 flex justify-center items-center my-2 mx-1">
+          <div className="rounded-full bg-black/30 backdrop-blur-sm w-28 h-28 md:mx-2 lg:mx-4 flex justify-center items-center my-2 mx-1 hover:scale-105 transition-all duration-300 hover:brightness-125">
             <img alt="postgres" src="/postgres.png" className="w-[80%] h-[80%] object-cover rounded-full" />
           </div>
-          <div className="rounded-full bg-black/30 backdrop-blur-sm w-28 h-28 md:mx-2 lg:mx-4 flex justify-center items-center my-2 mx-1">
+          <div className="rounded-full bg-black/30 backdrop-blur-sm w-28 h-28 md:mx-2 lg:mx-4 flex justify-center items-center my-2 mx-1 hover:scale-105 transition-all duration-300 hover:brightness-110">
             <img alt="aws" src="/aws.png" className="w-[80%] h-[80%] object-cover rounded-full" />
           </div>
         </div>
       </div>
-      <Link href="/projects" className="w-40 mx-auto h-16 mt-4 rounded-lg bg-purple-800/20 hover:bg-purple-800/40 transition-all duration-300 mb-32 items-center flex justify-center">
-        <p className="text-purple-600 text-3xl bebas">Projects</p>
-      </Link>
+      <Links />
       {/* <h2 className="select-none text-4xl mt-16 text-purple-700 font-bold text-center bg-black/30 backdrop-blur-sm w-32 py-2 mx-auto rounded-lg bebas">SKILLS</h2>
       <div className="w-2/5 mx-auto flex flex-col select-none px-5 mb-5 relative bg-black/30 backdrop-blur-sm my-8 py-2 rounded-lg">  
         <h6 className="text-xl text-purple-400"><span className="text-2xl font-bold">Front End:</span> JavaScript | TypeScript | NEXT.js | CSS | Tailwind</h6>
--- app/projects/PreviewCard.jsx
+++ app/projects/PreviewCard.jsx
@@ -4,13 +4,13 @@ export default function ProjectCard({ project }){
     const { name, description, projectUrl, frontEnd, backEnd, languages, image, util } = project;
 
     return (
-        <div className="w-[21rem] mb-10 transition-all duration-300 h-[23rem] rounded-lg bg-black/80 hover:bg-gray-600 pb-1 mx-auto px-2 relative pt-1 my-6">
-            <h2 className="mb-2 select-none w-fit left-1 bebas text-lg font-bold text-purple-500">{projectUrl ? name + " " : name}{projectUrl ? <a className="text-blue-500 text-base duration-300 transition-all font-semibold" target="_blank" href={projectUrl}>Link</a> : <span className="font-semibold text-base"> - Group Project</span>}</h2>
-            <Image loading="lazy" className="rounded-lg h-48 w-full object-cover" width={300} height={200} src={image} />
-            <a className="absolute mt-36 left-1 text-sm bg-black/30 font-bold text-purple-500 transition-all duration-200 hover:text-purple-600" target="_blank" rel="noopener noreferrer" href={frontEnd}>Front End Github</a>
-            <a className="absolute -bottom-[1.3rem] right-1 text-sm font-bold text-purple-500 transition-all duration-200 hover:text-purple-600" target="_blank" rel="noopener noreferrer" href={backEnd}>Back End Github</a>
-            <p className="select-none text-sm pl-1 pt-1 text-purple-300 font-semibold">{description}</p>
-            <p className="absolute bottom-1 select-none text-sm pl-1 pt-1 pb-[0.15rem] text-purple-400">{languages}</p>
+        <div className="w-[21rem] mb-28 transition-all duration-300 h-[23rem] rounded-lg bg-black/80 backdrop-blur-sm pb-1 mx-auto px-2 relative pt-1">
+            <h2 className="mb-2 ml-2 select-none w-fit left-1 bebas text-lg font-bold text-purple-300">{projectUrl ? name + " " : name}{projectUrl ? <a className="text-blue-500 hover:text-blue-400 text-base duration-300 transition-all font-semibold" target="_blank" href={projectUrl}>Link</a> : <span className="font-semibold text-base text-purple-400"> - Group Project</span>}</h2>
+            <Image loading="lazy" className="rounded-lg h-48 w-[95%] mx-auto object-cover" width={800} height={450} src={image} />
+            <a className="bebas absolute mt-36 left-1 bg-black/80 backdrop-blur-sm py-1 px-2 rounded-lg font-bold text-purple-500 transition-all duration-200 hover:text-purple-600" target="_blank" rel="noopener noreferrer" href={frontEnd}>Front End Github</a>
+            <a className="bebas absolute mt-36 right-1 font-bold bg-black/80 backdrop-blur-sm py-1 px-2 text-purple-500 transition-all duration-200 hover:text-purple-600" target="_blank" rel="noopener noreferrer" href={backEnd}>Back End Github</a>
+            <p className="select-none w-[95%] mx-auto text-sm pl-1 pt-1 text-purple-300 font-semibold">{description}</p>
+            <p className="absolute bottom-1 pl-2 pr-2 select-none text-sm pt-1 pb-[0.15rem] text-purple-400">{languages}</p>
         </div>
     )
 }
