4108fd6bcf52c8846d0321289866acfe2cadd05f
KonstantinStanmeyer <konstantin@stanmeyer.com>
2023-03-01 09:05:47 -0700

    anchor tag fix

--- app/page.jsx
+++ app/page.jsx
@@ -29,7 +29,7 @@ export default function HomePage(){
         <p className="text-purple-300 text-2xl my-12 font-bold w-4/5 sm:w-4/5 mx-auto select-none backdrop-blur-sm rounded-lg">
           <span className="bebas text-2xl sm:text-3xl text-purple-400 ">ABOUT ME: </span>I'm a full-stack software engineer with a passion for learning in all forms. I love to challenge myself, always striving to improve my process and approach. Experienced with multiple languages and frameworks including NEXT.js and Ruby on Rails. Avid runner and skateboarder.
         </p>
-        <p id="two" className="absolute top-[-6rem] invisible">anchor</p>
+        <p id="two" className="absolute top-[-6rem] sm:top-[-14rem] invisible">anchor</p>
       </div>
       <div className="flex flex-col lg:w-1/2 w-5/6 md:w-3/5 mx-auto h-[75vh] -mt-2 justify-center relative">
         <p id="three" className="absolute invisible -top-20">anchor</p>
